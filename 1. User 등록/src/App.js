a874c3a2eadc826032ed3d087f02fb1e57d8bc3e
ckstn <ckstn0777@naver.com>
2020-07-29 23:46:46 +0900

    v1.4.0
    Immer.JS를 통한 불변성 유지

--- 1. User 등록/src/App.js
+++ 1. User 등록/src/App.js
@@ -6,6 +6,8 @@ import React, {
   useReducer,
   createContext,
 } from 'react';
+import produce from 'immer';
+
 import './App.css';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
@@ -57,22 +59,19 @@ function reducer(state, action) {
         },
       };
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user),
-      };
+      return produce(state, (draft) => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user,
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
+      return produce(state, (draft) => {
+        const index = draft.users.findIndex((user) => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state;
   }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 	1-2. 최적화/성능 향상시키기(v1.1.0 ~ v1.1.1)  
 	1-3. UseReducer 사용(v1.2.0 ~ v1.2.1)  
 	1-4. Context API 사용(v1.3.0)  
-	1-5. Immer.js 사용  
+	1-5. Immer.js 사용(v1.4.0)  
   
 2. TodoList 만들기  
 	2-1. UI 만들기(Styled-components 방식사용)  
