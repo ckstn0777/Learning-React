7b54ff49747f1e89f93619d3517c4a3cb39e83a3
ckstn <ckstn0777@naver.com>
2020-07-29 22:28:22 +0900

    v1.0.3
    useState를 사용한 Basic 형태 1 - onToggle(토글기능) 추가

--- 1. User 등록/src/App.js
+++ 1. User 등록/src/App.js
@@ -25,16 +25,19 @@ function App() {
       id: 1,
       username: 'velopert',
       email: 'public.velopert@gmail.com',
+      active: true,
     },
     {
       id: 2,
       username: 'ckstn',
       email: 'ckstn@naver.com',
+      active: false,
     },
     {
       id: 3,
       username: 'tester',
       email: 'testert@daum.net',
+      active: false,
     },
   ]);
 
@@ -58,6 +61,14 @@ function App() {
     setUsers(users.filter((user) => user.id !== id));
   };
 
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user,
+      ),
+    );
+  };
+
   return (
     <>
       <CreateUser
@@ -66,7 +77,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
