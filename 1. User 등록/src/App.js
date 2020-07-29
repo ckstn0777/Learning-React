c405362ac97860f671aa35cb8cfdf12f86332c19
ckstn <ckstn0777@naver.com>
2020-07-29 22:17:53 +0900

    v1.0.1
    useState를 사용한 Basic 형태 1 - CreateUser(등록하기) 추가

--- 1. User 등록/src/App.js
+++ 1. User 등록/src/App.js
@@ -1,10 +1,26 @@
 // App.js
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
@@ -20,14 +36,35 @@ function App() {
       username: 'tester',
       email: 'testert@daum.net',
     },
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username: '',
+      email: '',
+    });
     nextId.current += 1;
   };
 
-  return <UserList users={users} />;
+  return (
+    <>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
