55fae93fcd3a15bde4fb4905b322341a316682c4
ckstn <ckstn0777@naver.com>
2020-07-31 00:12:33 +0900

    v3.0.0
    API 연동하기 1 - API 연동기본

---
+++ 3. API 요청하기/src/App.js
@@ -0,0 +1,10 @@
+// App.js
+import React from 'react';
+import './App.css';
+import Users from './Users';
+
+function App() {
+  return <Users />;
+}
+
+export default App;
---
+++ 3. API 요청하기/src/Users.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Users() {
+  // API를 요청하게 될때는 3가지 종류의 상태를 관리해야 하는데요.
+  // 1) 요청의 결과 2) 로딩 상태 3) 에러 입니다.
+  const [users, setUsers] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  // 처음 로딩될때만 실행되도록
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        // 초기화 및 요청 start
+        setUsers(null);
+        setError(null);
+        setLoading(true);
+
+        // 호출
+        const response = await axios.get(
+          'https://jsonplaceholder.typicode.com/users/',
+        );
+
+        setUsers(response.data); // 저장
+      } catch (e) {
+        setError(e);
+      }
+
+      setLoading(false); // 요청 종료
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return null;
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>
+          {user.username} ({user.name})
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Users;
