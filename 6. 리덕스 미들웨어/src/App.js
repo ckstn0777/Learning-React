38639e57fb8697deed62a4b137b16a075cb54def
ckstn <ckstn0777@naver.com>
2020-08-02 23:23:54 +0900

    v6.1.1
    리덕스 thunk 사용하기 3 - posts 불러오기

--- 6. 리덕스 미들웨어/src/App.js
+++ 6. 리덕스 미들웨어/src/App.js
@@ -2,11 +2,14 @@
 import React from 'react';
 import './App.css';
 import CounterContainer from './containers/CounterContainer';
+import PostListContainer from './containers/PostListContainer';
 
 function App() {
   return (
     <div>
       <CounterContainer />
+      <hr />
+      <PostListContainer />
     </div>
   );
 }
