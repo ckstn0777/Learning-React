da4cfd2af172a7645e4e569d8fc263a14dc7a452
ckstn <ckstn0777@naver.com>
2020-08-02 22:48:17 +0900

    v6.0.1
    리덕스 미들웨어 사용해보기 2

--- 6. 리덕스 미들웨어/src/index.js
+++ 6. 리덕스 미들웨어/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import myLogger from './middlewares/MyLogger';
 
-const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
+// 스토어에 미들웨어를 적용 할 때에는 applyMiddleware 라는 함수를 사용합니다.
+const store = createStore(rootReducer, applyMiddleware(myLogger)); // 스토어를 만듭니다.
 
 // Provider로 store를 넣어서 App 을 감싸게 되면
 // 우리가 렌더링하는 그 어떤 컴포넌트던지 리덕스 스토어에 접근 할 수 있게 된답니다.
