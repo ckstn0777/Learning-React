2620075be3a9551bccd675e6100d5b06fb8e8ac7
ckstn <ckstn0777@naver.com>
2020-08-03 21:31:07 +0900

    v8.0.2
    Typescript 사용하기 1 - useReducer로 상태관리

--- 8. 타입스크립트 사용/src/App.tsx
+++ 8. 타입스크립트 사용/src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import Counter from './Counter';
-import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
 function App() {
-  const onSubmit = (form: { name: string; description: string }) => {
-    console.log(form);
-  };
-
   return (
     <>
       <Counter />
       <hr />
-      <MyForm onSubmit={onSubmit} />
+      <ReducerSample />
     </>
   );
 }
