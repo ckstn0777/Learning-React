51c7442b2144ac4fb46fd9aa066ce99a36cf0df0
ckstn <ckstn0777@naver.com>
2020-08-03 21:17:10 +0900

    v8.0.1
    Typescript 사용하기 1 - useState로 상태관리

--- 8. 타입스크립트 사용/src/App.tsx
+++ 8. 타입스크립트 사용/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import Greetings from './Greetings';
+import Counter from './Counter';
+import MyForm from './MyForm';
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(name);
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
   };
-  return <Greetings name="리액트" optional="옵션" onClick={onClick} />;
+
+  return (
+    <>
+      <Counter />
+      <hr />
+      <MyForm onSubmit={onSubmit} />
+    </>
+  );
 }
 
 export default App;
