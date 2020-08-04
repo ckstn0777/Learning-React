1f27c1fb4c3e7c462f1261c5b59c5bcb033c7095
ckstn <ckstn0777@naver.com>
2020-08-04 22:11:53 +0900

    v11.1.2
    JWT를 통한 인증 시스템 구현하기 2 - 토큰 재발급 및 로그아웃

--- 11. JWT를 이용한 인증시스템 구현/src/api/auth/auth.ctrl.js
+++ 11. JWT를 이용한 인증시스템 구현/src/api/auth/auth.ctrl.js
@@ -107,4 +107,10 @@ export const check = async (ctx) => {
 };
 
 // 로그아웃
-export const logout = async (ctx) => {};
+/*
+POST /api/auth/logout
+*/
+export const logout = async (ctx) => {
+  ctx.cookies.set("access_token");
+  ctx.status = 204; // No Content
+};
