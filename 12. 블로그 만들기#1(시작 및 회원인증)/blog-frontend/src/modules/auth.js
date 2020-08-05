eebe7f2abf8472f283d1aadf4b0b00c75d198c34
ckstn <ckstn0777@naver.com>
2020-08-05 23:08:56 +0900

    v12.2.0
    리덕스로 폼 상태관리 3 - auth 모듈 수정 및 LoginForm 적용

--- 12. 블로그 만들기#1(시작 및 회원인증)/blog-frontend/src/modules/auth.js
+++ 12. 블로그 만들기#1(시작 및 회원인증)/blog-frontend/src/modules/auth.js
@@ -1,18 +1,51 @@
 import { createAction, handleActions } from 'redux-actions'; // redux를 더 편하게 사용하기(p.457 참고)
+import produce from 'immer';
 
 // 액션 객체
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
 // 액션 생성함수
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ form, key, value }) => ({
+    form, // register, login
+    key, // username, password, passwordConfirm
+    value, // 실제 바꾸려는 값
+  }),
+);
+// export const changeField = ({form, key, value}) => ({type: CHANGE_FIELD, {form, key, value}}) // createAction 안썼을경우
+
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form); // register / login
+// export const initializeForm = (form) => ({type: CHANGE_FIELD, form}) // createAction 안썼을경우
 
 // 초기상태
-const initialState = {};
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    username: '',
+    password: '',
+  },
+};
 
 // 리듀서
 const auth = handleActions(
   {
-    [SAMPLE_ACTION]: (state, action) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(
+        state,
+        (draft) => {
+          draft[form][key] = value;
+        }, // 예: state.register.username을 바꾼다
+      ),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
   },
   initialState,
 );
