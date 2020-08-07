ba097cecda721a14e237549e98964d3dd9d80b6a
ckstn <ckstn0777@naver.com>
2020-08-07 12:30:45 +0900

    v13.1.0
    리덕스로 글쓰기 상태관리 2 - 포스트 title, body 상태관리

--- 13. 블로그 만들기#2(글쓰기)/blog-frontend/src/modules/index.js
+++ 13. 블로그 만들기#2(글쓰기)/blog-frontend/src/modules/index.js
@@ -3,11 +3,13 @@ import auth, { authSaga } from './auth';
 import loading from './loading';
 import user, { userSaga } from './user';
 import { all } from 'redux-saga/effects';
+import write from './write';
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
+  write,
 });
 
 export function* rootSaga() {
